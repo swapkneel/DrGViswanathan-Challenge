c11fa8b3df62ccb7eeb9bfeea0c51e18e19a969a
Swapnil <swapnilkumarmeerut@gmail.com>
2026-06-11 19:25:22 +0530

    Day 4- MajorityElement

--- Day 4/MajorityElement.java
+++ Day 4/MajorityElement.java
@@ -1,18 +1,22 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
+    public int majorityElement(int[] nums) {
 
-        int insertPos = 0;
+        int candidate = 0;
+        int count = 0;
 
         for(int num : nums){
-            if(num != 0){
-                nums[insertPos] = num;
-                insertPos++;
+
+            if(count == 0){
+                candidate = num;
+            }
+
+            if(num == candidate){
+                count++;
+            } else {
+                count--;
             }
         }
 
-        while(insertPos < nums.length){
-            nums[insertPos] = 0;
-            insertPos++;
-        }
+        return candidate;
     }
 }
